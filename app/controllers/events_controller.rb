32dc715b4fef6f5f5994deb567a1757aa098e250
Michael A. Holmes <holmes.m.andrew@gmail.com>
2018-04-12 14:59:19 -0500

    Show and Index Pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
+    before_action :find_event, only: [:show, :edit, :update, :destroy]
 
     def index
+    end
+
+    def show
     end
 
     def new
@@ -9,11 +13,21 @@ class EventsController < ApplicationController
 
     def create
         @event = Event.new(event_params)
+
+        if @event.save
+            redirect_to @event, notice: "Great! The event was posted!"
+        else
+            render 'new'
+        end
     end
 
     private
 
     def event_params
         params.require(:event).permit(:title, :description)
+    end
+
+    def find_event
+        @event = Event.find(params[:id])
     end
 end
