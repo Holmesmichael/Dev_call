72b8872d3a1739de669af1038d1528791904a33c
Michael A. Holmes <holmes.m.andrew@gmail.com>
2018-04-12 15:11:58 -0500

    Edit, update and destroy actions and new files

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
     before_action :find_event, only: [:show, :edit, :update, :destroy]
 
     def index
+        @events = Event.all.order("created_at DESC")
     end
 
     def show
@@ -19,6 +20,22 @@ class EventsController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def edit 
+    end
+
+    def update 
+        if @event.update(event_params)
+            redirect_to @event, notice: "Congrats, Event was updated!"
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy 
+        @event.destroy
+        redirect_to root_path
     end
 
     private
